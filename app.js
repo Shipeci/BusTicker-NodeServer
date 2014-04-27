a2621cbd37e2e684c671dc0702499e9127569e32
tdhsmith <contact@tdhsmith.com>
2014-04-26 22:27:03 -0500

    Add stop finding API
    Add new responses to find nearest stops to a coord with /stop/ROUTE/DIR/LAT/LNG. Relocated predictions to /predictions/STOPID, duplicated at /times/STOPID until we settle on good verbs and nouns.

--- app.js
+++ app.js
@@ -27,18 +27,94 @@ app.use(function(err, req, res, next){
 });
 */
 
-app.get('/stop/:stopID/:busDir', function(req, res){
-  var apikey = "HWbvqrH8fJgqZ2PPRS6UyfBaW";
-  var rt = 21;
-  var stpid = req.params.stopID;
-  if (req.params.busDir in )
-  var pathString = '/bustime/api/v1/getpredictions?key='+apikey+'&rt='+rt+'&stpid='+stpid;
-  if (direction) {pathString += '&dir='+direction;}
+var apikey = "HWbvqrH8fJgqZ2PPRS6UyfBaW"; //get this out of here
+
+var haversineDistance = function(lat1, lon1, lat2, lon2) {
+  // thanks to http://andrew.hedges.name/experiments/haversine/
+  var d2r = function (degrees) {return degrees*Math.PI/180;}
+  lat1 = d2r(lat1);
+  lat2 = d2r(lat2);
+  lon1 = d2r(lon1);
+  lon2 = d2r(lon2);
+  var dlon = lon2 - lon1;
+  var dlat = lat2 - lat1;
+  var a = Math.pow((Math.sin(dlat/2)),2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow((Math.sin(dlon/2)),2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a) );
+  return (3958.7 * c);
+}
+
+var findNearestStop = function(req, res){
+  var rt = req.params.route; // need to validate stuff!
+  var lat = parseFloat(req.params.lat);
+  var lng = parseFloat(req.params.lng);
+  var dir = req.params.busDir;
+  var pathString = '/bustime/api/v1/getstops?key='+apikey+'&rt='+rt+"&dir="+dir;
 
   var options = {
     host : 'realtime.ridemcts.com',
     path : pathString,
-    //port : 80,
+    method : 'GET'
+  }
+
+  var request = http.request(options, function(response){
+    var body = ""
+    response.on('data', function(data) {
+      body += data;
+    });
+    response.on('end', function() {
+      parseXML(body, function(err, result) {
+        var data = result['bustime-response'];
+        var stops = data['stop'];
+        var numStops = stops.length;
+        var unsortedStops = [];
+        //console.log("Distances to stops");
+        for (var i = 0; i<numStops; i++) {
+          unsortedStops.push({});
+          unsortedStops[i].responseObj = {};
+          unsortedStops[i].responseObj.stopID = parseInt(stops[i].stpid[0]);
+          unsortedStops[i].responseObj.stopName = stops[i].stpnm[0];
+          unsortedStops[i].responseObj.distance = haversineDistance(lat,lng,stops[i].lat[0],stops[i].lon[0]);
+          //console.log("Stop "+stops[i].stpnm[0]+" is "+unsortedStops[i].responseObj.distance);
+        }
+
+        var sortedStops = [];
+        var shortestDist, currentDist, closestIndex, len, currentObj;
+        while (unsortedStops.length > 1) {
+          len = unsortedStops.length;
+          shortestDist = 100000;
+          for (var i = 0; i<len; i++) {
+            currentDist = unsortedStops[i].responseObj.distance
+            if (currentDist < shortestDist) {
+              shortestDist = currentDist;
+              closestIndex = i;
+            }
+          }
+          currentObj = unsortedStops.splice(closestIndex, 1)[0].responseObj;
+          //console.log(currentObj);
+          sortedStops.push(currentObj);
+        }
+        res.json(sortedStops);
+      });
+    });
+  });
+  request.on('error', function(e) {
+    console.log('Problem with request: ' + e.message);
+  });
+  request.end();
+}
+
+var getPredictions = function(req, res){
+  var rt = 21;
+  var stpid = req.params.stopID;
+  //var checkDir = false;
+  var pathString = '/bustime/api/v1/getpredictions?key='+apikey+'&rt='+rt+'&stpid='+stpid;
+  /* if (["EAST", "WEST", "NORTH", "SOUTH"].indexOf(req.params.busDir) > -1) {
+    checkDir = true;
+  } */
+
+  var options = {
+    host : 'realtime.ridemcts.com',
+    path : pathString,
     method : 'GET'
   }
 
@@ -54,14 +130,12 @@ app.get('/stop/:stopID/:busDir', function(req, res){
         var numPred = predictions.length;
         var jsonResponse = [];
         for (var i = 0; i<numPred; i++) {
-          //console.log(predictions[i]);
           jsonResponse.push({});
-          //jsonResponse[i].order = i;
           jsonResponse[i].age = 0;
           jsonResponse[i].prediction = predictions[i].prdtm[0];
-          jsonResponse[i].delayed = (predictions[i].dly ? true : false);
-          jsonResponse[i].route = predictions[i].rt;
-          jsonResponse[i].direction = predictions[i].rtdir;
+          jsonResponse[i].delayed = (predictions[i].dly && predictions[i].dly[0] ? true : false);
+          jsonResponse[i].route = predictions[i].rt[0];
+          jsonResponse[i].direction = predictions[i].rtdir[0];
         }
         res.json(jsonResponse);
       });
@@ -72,7 +146,11 @@ app.get('/stop/:stopID/:busDir', function(req, res){
     console.log('Problem with request: ' + e.message);
   });
   request.end();
-});
+};
+
+app.get('/predictions/:stopID', getPredictions);
+app.get('/times/:stopID', getPredictions);
+app.get('/stop/:route/:busDir/:lat/:lng', findNearestStop);
 
 /*
 app.use(function(req, res) { //direct 404s to /views/404.html
